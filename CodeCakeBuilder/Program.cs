b9cc353f58344c752052884ddfb1798bd5e0e667
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-08-25 17:40:50 +0200

    CodeCake should return the error code (for appveyor to know that something gone wrong).

--- CodeCakeBuilder/Program.cs
+++ CodeCakeBuilder/Program.cs
@@ -10,19 +10,20 @@ namespace CodeCake
 {
     class Program
     {
-        static void Main( string[] args )
+        static int Main( string[] args )
         {
             var app = new CodeCakeApplication();
             var a = new List<string>( args );
             int idx = a.FindIndex( s => s.Equals( "-nowait", StringComparison.OrdinalIgnoreCase ) );
             if( idx >= 0 ) a.RemoveAt( idx );
-            app.Run( a.ToArray() );
+            int result = app.Run( a.ToArray() );
             Console.WriteLine();
             if( idx < 0 )
             {
-                Console.WriteLine( "Hit any key to exit. (Use -nowait parameter to end immediately)" );
+                Console.WriteLine( "Hit any key to exit. (Use -nowait parameter to exit immediately)" );
                 Console.ReadKey();
             }
+            return result;
         }
     }
 }
