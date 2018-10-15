10232d1cb484fdddc205bfe7300b23cc89a8a765
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-10-15 12:10:41 +0200

    Now uses time-based computed by CSemVer.Net.

--- SimpleGitVersion.Core/RepositoryInfo/CIReleaseInfo.cs
+++ SimpleGitVersion.Core/RepositoryInfo/CIReleaseInfo.cs
@@ -61,8 +61,8 @@ namespace SimpleGitVersion
             if( ciVersionMode == CIBranchVersionMode.ZeroTimed || actualBaseTag == null )
             {
                 DateTime timeRelease = commit.Committer.When.ToUniversalTime().UtcDateTime;
-                ciBuildVersion = SVersion.Parse( CreateSemVerZeroTimed( ciBuildName, timeRelease, actualBaseTag?.ToString() ) );
-                ciBuildVersionNuGet = SVersion.Parse( CreateNuGetZeroTimed( ciBuildName, timeRelease ), false );
+                ciBuildVersion = SVersion.Parse( CIBuildDescriptor.CreateSemVerZeroTimed( ciBuildName, timeRelease, actualBaseTag?.ToString() ) );
+                ciBuildVersionNuGet = SVersion.Parse( CIBuildDescriptor.CreateShortFormZeroTimed( ciBuildName, timeRelease ), false );
             }
             else
             {
@@ -84,59 +84,6 @@ namespace SimpleGitVersion
             }
             Debug.Assert( ciBuildVersion == null || errors.Length == 0 );
             return ciBuildVersion != null ? new CIReleaseInfo( ciBaseTag, info.BelowDepth, ciBuildVersion, ciBuildVersionNuGet ) : null;
-        }
-
-        /// <summary>
-        /// Creates the ZeroTimed NuGetV2 version string.
-        /// </summary>
-        /// <param name="ciBuildName">The BuildName string (typically "develop").</param>
-        /// <param name="timeRelease">The utc date time of the release.</param>
-        /// <returns>A NuGetV2 O.O.O-- version string.</returns>
-        public static string CreateNuGetZeroTimed( string ciBuildName, DateTime timeRelease )
-        {
-            if( string.IsNullOrWhiteSpace( ciBuildName ) ) throw new ArgumentException( nameof( ciBuildName ) );
-            DateTime baseTime = new DateTime( 2015, 1, 1, 0, 0, 0, DateTimeKind.Utc );
-            if( timeRelease < baseTime ) throw new ArgumentException( $"Must be at least {baseTime}.", nameof( timeRelease ) );
-            string ciBuildVersionNuGet;
-            TimeSpan delta200 = timeRelease - new DateTime( 2015, 1, 1, 0, 0, 0, DateTimeKind.Utc );
-            Debug.Assert( Math.Log( 1000 * 366 * 24 * 60 * (long)60, 62 ) < 7, "Using Base62: 1000 years in seconds on 7 chars!" );
-            long second = (long)delta200.TotalSeconds;
-            string b62 = ToBase62( second );
-            string ver = new string( '0', 7 - b62.Length ) + b62;
-            ciBuildVersionNuGet = string.Format( "0.0.0--{0}-{1}", ciBuildName, ver );
-            return ciBuildVersionNuGet;
-        }
-
-        /// <summary>
-        /// Creates the ZeroTimed SemVer version string. The <paramref name="actualBaseTag"/>, if not null, is appended 
-        /// as a suffix (Build metadata).
-        /// </summary>
-        /// <param name="ciBuildName">The BuildName string (typically "develop").</param>
-        /// <param name="timeRelease">The utc date time of the release.</param>
-        /// <param name="actualBaseTag">An optional base release that will be added as build metadata.</param>
-        /// <returns>A SemVer O.O.O--ci version string.</returns>
-        public static string CreateSemVerZeroTimed( string ciBuildName, DateTime timeRelease, string actualBaseTag = null )
-        {
-            if( string.IsNullOrWhiteSpace( ciBuildName ) ) throw new ArgumentException( nameof( ciBuildName ) );
-            var name = string.Format( "0.0.0--ci-{0}.{1:yyyy-MM-ddTHH-mm-ss-ff}", ciBuildName, timeRelease );
-            return name + (actualBaseTag != null ? "+v" + actualBaseTag : null);
-        }
-
-        static string ToBase62( long number )
-        {
-            // Naïve implementation that does the job.
-            var alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            var n = number;
-            long basis = 62;
-            var ret = "";
-            while( n > 0 )
-            {
-                long temp = n % basis;
-                ret = alphabet[(int)temp] + ret;
-                n = (n / basis);
-
-            }
-            return ret;
         }
     }
 
